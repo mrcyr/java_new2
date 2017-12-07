493b35124af4a2ca7e0c8bcfbfa58843922c96de
mrcyr <ionov.stanislav@gmail.com>
2017-12-07 18:03:38 +0300

    Добавлен DataProvider для контактов и взятие данных их файла

--- addressbook-web-test/src/test/java/ru/addressbook/tests/ContactAdd.java
+++ addressbook-web-test/src/test/java/ru/addressbook/tests/ContactAdd.java
@@ -1,25 +1,47 @@
 package ru.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.addressbook.model.ContactData;
 import ru.addressbook.model.Contacts;
+import ru.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
 import static org.testng.Assert.*;
 
 public class ContactAdd extends Testbase {
 
-  @Test(enabled = true)
-  public void ContactAddTest() {
+  @DataProvider
+  public Iterator<Object[]> validContacts() throws IOException {
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new ContactData().withFirstname(split[0])
+              .withLastname(split[1])
+              .withAddress(split[2])
+              .withMobNumber(split[3])
+              .withEmail1(split[4])});
+      line = reader.readLine();
+    }
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validContacts")
+  public void ContactAddTest(ContactData contacts) {
     app.goTo().home();
     Contacts before = app.contact().all();
     File photo = new File("src/test/resources/7Lj6myuF0cA.jpg");
-    ContactData contacts = new ContactData()
-            .withFirstname("Саша").withLastname("Иванов").withAddress("Питер")
-            .withMobNumber("123123123").withHomeNumber("123123").withEmail1("pey123@mail.ru").withEmail2("asdasd@asdsd.ru")
-            .withPhoto(photo);
     app.contact().create(contacts);
     assertEquals(app.contact().count(), before.size() + 1);
     Contacts after = app.contact().all();
